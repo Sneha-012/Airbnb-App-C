900ad85cf46fae91008892f9175ef3a24b0f776f
Sneha Gupta <snehagpta015@gmail.com>
2026-07-06 00:40:22 +0530

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/sneha/airbnbAppC/AirbnbAppCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.airbnbAppC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbAppCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
