cf44b280bd25ecf43a8384d90df4acc303ba71ca
Sneha Gupta <snehagpta015@gmail.com>
2026-07-17 22:01:02 +0530

    feat: add search for getting property's minimum room price

--- src/main/java/com/sneha/airbnbAppC/AirbnbAppCApplication.java
+++ src/main/java/com/sneha/airbnbAppC/AirbnbAppCApplication.java
@@ -2,8 +2,10 @@ package com.sneha.airbnbAppC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AirbnbAppCApplication {
 
 	public static void main(String[] args) {
