96ea240e4d3aeabba45bad617fbeb626e9e1f9fc
Sneha Gupta <snehagpta015@gmail.com>
2026-07-27 23:12:05 +0530

    feat: adding changes

--- src/main/java/com/sneha/airbnbAppC/repository/BookingRepository.java
+++ src/main/java/com/sneha/airbnbAppC/repository/BookingRepository.java
@@ -1,7 +1,15 @@
 package com.sneha.airbnbAppC.repository;
 
 import com.sneha.airbnbAppC.entity.Booking;
+import com.sneha.airbnbAppC.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Optional<Booking> findByPaymentSessionId(String sessionId);
+
+    List<Booking> findByProperty(Property property);
 }
--- src/main/java/com/sneha/airbnbAppC/service/BookingService.java
+++ src/main/java/com/sneha/airbnbAppC/service/BookingService.java
@@ -4,6 +4,8 @@ import com.sneha.airbnbAppC.dto.booking.BookingRequestDto;
 import com.sneha.airbnbAppC.dto.booking.BookingResponseDto;
 import com.sneha.airbnbAppC.dto.guest.GuestRequestDto;
 import com.sneha.airbnbAppC.dto.guest.GuestResponseDto;
+import com.sneha.airbnbAppC.entity.enums.BookingStatus;
+import com.stripe.model.Event;
 import jakarta.validation.Valid;
 import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
@@ -16,4 +18,15 @@ public interface BookingService {
     BookingResponseDto initialiseBooking(BookingRequestDto bookingRequestDto);
 
     BookingResponseDto addGuests(Long bookingId, List<GuestRequestDto> guestRequestDtoList);
+
+
+    String initiatePayments(Long bookingId);
+
+    void capturePayment(Event event);
+
+    void cancelBooking(Long bookingId);
+
+    BookingStatus getBookingStatus(Long bookingId);
+
+    List<BookingResponseDto> getAllBookingsByPropertyId(Long propertyId);
 }
